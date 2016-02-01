9c6ea34fcbd7b4a13d8f548602e7e874defa529e
Tree Casiano <tree.pdx@gmail.com>
2016-02-01 06:55:08 -0800

    Initial commit.

---
+++ js/calculator.js
@@ -0,0 +1,23 @@
+// calculator.js
+
+( function( $ ) {
+
+var answer = "";
+var calc = "";
+
+
+// assign event handler to buttons with switch statements for the options
+
+// if button value is "=" use eval() method to evaluate the expression
+
+// after eval(), clear text field and replace with answer
+
+
+})( jQuery );
+
+
+
+
+
+
+
